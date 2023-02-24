ff7bf4f736ac122a5c114d4c26ea64397bd7982f
Tilahun <Tilahun362@gmail.com>
2023-02-24 13:14:33 -0500

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcase/TC_003.java
@@ -0,0 +1,27 @@
+package testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TC_003 {
+	
+	
+   @Test
+	public void testcase1() {
+	   WebDriverManager.chromedriver().setup();
+	   WebDriver driver = new ChromeDriver();  // Starting the Browser
+	   driver.get("http://www.facebook.com");
+	   driver.findElement(By.id("email")).sendKeys("Hello");
+	   driver.findElement(By.id("pass")).sendKeys("Hello");
+	   driver.quit();
+	   
+	  
+		
+	}
+	
+}
